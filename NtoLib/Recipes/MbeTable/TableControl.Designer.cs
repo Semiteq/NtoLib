6868f89e81e4f898a0293e1956d5aec7a4e8d4be
UltraMurlock <egorplettnev00@gmail.com>
2024-08-15 13:56:41 +0300

    Кнопкам таблицы рецептов МБЕ добавлены иконки

--- NtoLib/Recipes/MbeTable/TableControl.Designer.cs
+++ NtoLib/Recipes/MbeTable/TableControl.Designer.cs
@@ -21,7 +21,8 @@ namespace NtoLib.Recipes.MbeTable
 
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TableControl));
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button_add_after = new System.Windows.Forms.Button();
             this.button_add_before = new System.Windows.Forms.Button();
@@ -43,14 +44,14 @@ namespace NtoLib.Recipes.MbeTable
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Window;
-            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
-            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlText;
-            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
-            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
-            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
             this.dataGridView1.Location = new System.Drawing.Point(3, 3);
             this.dataGridView1.MultiSelect = false;
             this.dataGridView1.Name = "dataGridView1";
@@ -66,12 +67,13 @@ namespace NtoLib.Recipes.MbeTable
             // button_add_after
             // 
             this.button_add_after.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_add_after.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_add_after.BackgroundImage")));
+            this.button_add_after.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button_add_after.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button_add_after.Location = new System.Drawing.Point(826, 483);
             this.button_add_after.Name = "button_add_after";
             this.button_add_after.Size = new System.Drawing.Size(43, 40);
             this.button_add_after.TabIndex = 13;
-            this.button_add_after.Text = "ADD+";
             this.button_add_after.UseVisualStyleBackColor = true;
             this.button_add_after.Visible = false;
             this.button_add_after.Click += new System.EventHandler(this.ClickButton_AddLineAfter);
@@ -79,12 +81,13 @@ namespace NtoLib.Recipes.MbeTable
             // button_add_before
             // 
             this.button_add_before.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_add_before.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_add_before.BackgroundImage")));
+            this.button_add_before.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button_add_before.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button_add_before.Location = new System.Drawing.Point(777, 483);
             this.button_add_before.Name = "button_add_before";
             this.button_add_before.Size = new System.Drawing.Size(43, 40);
             this.button_add_before.TabIndex = 12;
-            this.button_add_before.Text = "ADD-";
             this.button_add_before.UseVisualStyleBackColor = true;
             this.button_add_before.Visible = false;
             this.button_add_before.Click += new System.EventHandler(this.ClickButton_AddLineBefore);
@@ -92,12 +95,13 @@ namespace NtoLib.Recipes.MbeTable
             // button_del
             // 
             this.button_del.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_del.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_del.BackgroundImage")));
+            this.button_del.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
             this.button_del.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button_del.Location = new System.Drawing.Point(728, 483);
             this.button_del.Name = "button_del";
             this.button_del.Size = new System.Drawing.Size(43, 40);
             this.button_del.TabIndex = 11;
-            this.button_del.Text = "DEL";
             this.button_del.UseVisualStyleBackColor = true;
             this.button_del.Visible = false;
             this.button_del.Click += new System.EventHandler(this.ClickButton_Delete);
@@ -105,13 +109,14 @@ namespace NtoLib.Recipes.MbeTable
             // button_save
             // 
             this.button_save.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_save.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_save.BackgroundImage")));
+            this.button_save.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button_save.Enabled = false;
             this.button_save.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button_save.Location = new System.Drawing.Point(924, 483);
             this.button_save.Name = "button_save";
             this.button_save.Size = new System.Drawing.Size(41, 40);
             this.button_save.TabIndex = 9;
-            this.button_save.Text = "SAVE";
             this.button_save.UseVisualStyleBackColor = true;
             this.button_save.Visible = false;
             this.button_save.Click += new System.EventHandler(this.ClickButton_Save);
@@ -119,13 +124,14 @@ namespace NtoLib.Recipes.MbeTable
             // button_open
             // 
             this.button_open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_open.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_open.BackgroundImage")));
+            this.button_open.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button_open.Enabled = false;
             this.button_open.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.button_open.Location = new System.Drawing.Point(875, 483);
             this.button_open.Name = "button_open";
             this.button_open.Size = new System.Drawing.Size(43, 40);
             this.button_open.TabIndex = 8;
-            this.button_open.Text = "OPEN";
             this.button_open.UseVisualStyleBackColor = true;
             this.button_open.Click += new System.EventHandler(this.ClickButton_Open);
             // 
