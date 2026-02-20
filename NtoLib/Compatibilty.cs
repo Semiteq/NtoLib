00cb76e35bd089be4c2bb17acef98f9200b6c5d6
Oleg <18466855+EvaTheSalmon@users.noreply.github.com>
2026-02-20 13:25:34 +0300

    Perfrormed IJ Cleanup/Reformat for NtoLib project files. Applied new editorconfig

--- NtoLib/Compatibilty.cs
+++ NtoLib/Compatibilty.cs
@@ -2,4 +2,6 @@
 
 // This class is used to enable C# 9.0 features (like init properties) in projects that target .NET Framework 4.8 or earlier.
 
-internal static class IsExternalInit { }
+internal static class IsExternalInit
+{
+}
