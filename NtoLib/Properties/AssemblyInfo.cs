72a995e70100922c717a2ff74329c8d6ee94303b
Oleg <18466855+EvaTheSalmon@users.noreply.github.com>
2026-02-24 11:48:21 +0300

    Updated version

--- NtoLib/Properties/AssemblyInfo.cs
+++ NtoLib/Properties/AssemblyInfo.cs
@@ -18,5 +18,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("1.11.0")]
 
 // beta marking below
-[assembly: AssemblyProduct("NtoLib SDK 1.11.0-beta1")]
-[assembly: AssemblyInformationalVersion("1.11.0-beta1")]
+[assembly: AssemblyProduct("NtoLib SDK 1.11.0-beta2")]
+[assembly: AssemblyInformationalVersion("1.11.0-beta2")]
